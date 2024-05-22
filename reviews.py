fc7c10a6ce6dcd93f72697f9943345a4a3b3efa1
jduck11 <jjhowa02@icloud.com>
2024-05-21 21:51:03 -0400

    wrw

--- reviews.py
+++ reviews.py
@@ -1,2 +1,9 @@
-# add your code here
+import pandas as pd
+
+df = pd.read_csv('data/wingmag-data-130k-v2.csv.zip')
+summary_df = df. groupby('country').agg({'title': 'count', 'points': 'mean'}).round(1)
+summary_df = summary_df.rename(colums ={'title': 'count'}).reset_index()
+summary_df = summary_df.sort_values('count', ascending=False)
+summary_df.to_cvs('data/reviews-per-country.cvs', index=False)
+
 
